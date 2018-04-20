fdbe0765ba5d2370d708ae0eb0d6e000d871ed8f
suryansh tiwari <f2013777@pilani.bits-pilani.ac.in>
2018-04-20 07:39:34 +0530

    rubique way of routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
 
-module.exports = router;
+/* GET home page. */
+const home = require('./home');
+const posts = require('./posts');
+
+module.exports = function(app) {
+    // app.use(openEndpoints());
+    router.use('/', home);
+    router.use('/posts', posts);
+    return router;
+}
+
