6c37c9272be94af17df227cb5ab494e9f784baf5
Shubham Garg <shubhamgrg04@gmail.com>
2018-04-28 20:50:48 +0530

    ui improvements and adding classifieds

--- frontend/src/containers/home/index.js
+++ frontend/src/containers/home/index.js
@@ -6,14 +6,31 @@ import {connect} from 'react-redux';
 import fetchPosts from '../../actions/fetch-posts';
 import {BrowserRouter, Link, Route, Switch} from 'react-router-dom';
 import {Field, reduxForm} from 'redux-form';
+import homeImage from './img.svg';
 
 class Home extends React.Component {
     render(){
         var channelPosts = this.props.channelPosts;
+        var classifieds = [
+            {
+                title: "Software Engineering Intership @ Microsoft",
+                description: "Microsoft is looking for interns for summer of 2018 for software development roles"
+            },
+            {
+                title: "Research Opportunities @ TA Deutcheland",
+                description: "TA Deutcheland is accepting applications for its annual intake of student researchers who will work for 3 months in the summer."
+            },
+            {
+                title: "Anita Borg Women Hackathon",
+                description: "Anita Borg Hackathon is organized annualy to promote women in technology"
+            }
+        ];
+
+        
         return(
             <div>
                 <HomeJumbotron />
-                <div className="container-fluid mt-5">
+                <div className="container mt-5">
                     <div className="row">
                         <Channels />
                         <Route path={`/:channel/newpost`} render={ (props) => (
@@ -24,6 +41,7 @@ class Home extends React.Component {
                                 <Posts {...props} channelPosts={channelPosts}  fetchPosts={this.props.fetchPosts} />
                             );
                         } } />
+                        <Classifieds classifieds={classifieds} />
                     </div>
                 </div>
             </div>
@@ -33,11 +51,24 @@ class Home extends React.Component {
  
 function HomeJumbotron(props) {
     return (
-    <div className="jumbotron mb-0 mt-5 bg-white">
-        <div className="container-fluid">
-            <p className="display-4" style={{color:"#d2d8ea"}}>Ask and Discuss with fellow BITSians</p>
+        <div className="jumbotron mb-0 mt-5 bg-white">
+            <div className="container">
+                <div className="d-flex flex-row">
+                    <div className="col-md-6 m-2" >
+                        <p className="heading">College Communities</p>
+                        <p className="sub-heading">
+                            Interact with fellow students and do amazing things. Hoods is a platform to connect college students to discuss ideas, 
+                            exchange things, make plans and connect with like minded people. There exist reddit, tumble etc. but they are not closed
+                            communites which doesn't allow to effectively discuss local issues.
+                        </p>
+                        <a href="/signup" className="btn btn-primary btn-custom pl-4 pr-4 pt-2 pb-2 mt-2">SIGN UP</a>
+                    </div>
+                    <div className="col-md-6 m-2" >
+                        <img className="float-right my-auto" style={{width: "300px"}} src={homeImage} />
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
     );
 }
 
@@ -89,15 +120,22 @@ class Posts extends React.Component {
             var postItems = posts.map((post) => {
                 return (
                     <div className="post d-flex flex-row p-4 m-0">
-                        <div className="d-flex flex-column" style={{color:"#7f7f7f"}}>
-                            <div className="p-0  post-upvote text-center"><a href="upvote()"><FontAwesomeIcon icon={fachevronup} size="3x"/></a></div>
-                            <div className="h5 p-0 post-votes text-center">{post.votes}</div>
-                            <div className="p-0 post-downvote text-center"><a href="downvote()"><FontAwesomeIcon icon={fachevrondown} size="3x"/></a></div>
+                        <div className="d-flex flex-column my-auto">
+                            <div className="justify-content-center align-self-center">
+                                <div className="p-0  post-upvote text-center"><a href="upvote()"><FontAwesomeIcon icon={fachevronup} size="3x"/></a></div>
+                                <div className="h5 p-0 post-votes text-center">{post.votes}</div>
+                                <div className="p-0 post-downvote text-center"><a href="downvote()"><FontAwesomeIcon icon={fachevrondown} size="3x"/></a></div>
+                            </div>
                         </div>
-                        <div className="ml-4 p-2">
+                        <div className="ml-4 p-2 my-auto">
+                            <p className="post-details" ><a className="post-author"  href="profiles/454df8">{post.author}</a></p>
                             <h4 className="post-heading">{post.title}</h4>
-                            <p className="post-details" >posted 3 days ago by <a className="post-author"  href="profiles/454df8">{post.author}</a></p>
-                            <a className="post-comments mt-2">{post.comments} Comments</a>
+                            <p className="post-details" >{post.description}</p>
+                            <p className="post-actions">
+                                <a><span className="color-primary">{post.comments} ANSWERS</span></a>
+                                <a className="ml-2">SHARE</a>
+                                <span className="time-ago ml-2">7 HOURS AGO</span>
+                            </p>
                         </div>
                     </div>
                 );
@@ -108,10 +146,41 @@ class Posts extends React.Component {
         }
         
         return (
-            <div className="posts col-md-5">
-                <Link to={"/" + this.props.match.params.channel + "/newpost"} role="button" class="btn float-right add-new mb-3" >New Post</Link>
+            <div className="posts col-md-7">
+                <Link to={"/" + this.props.match.params.channel + "/newpost"} role="button" class="btn btn-primary btn-custom float-right mb-3" >New Post</Link>
                 <div class="clearfix"></div>
                 {postItems}
+            </div>
+        );
+    }
+    
+}
+
+class Classifieds extends React.Component {
+    render() {
+        var classifieds = this.props.classifieds;
+        
+        if(classifieds!=null) {
+            var classifiedItems = classifieds.map((classified) => {
+                return (
+                    <div class="post d-flex flex-column p-2 m-0">
+                        <div class="ml-2 p-2 my-auto">
+                            <h4 class="classified-heading">{classified.title}</h4>
+                            <p class="post-actions">
+                                <a class="">SHARE</a>
+                                <span class="time-ago ml-2">7 HOURS AGO</span>
+                            </p>
+                        </div>
+                    </div>
+                );
+            });
+        } else {
+            console.log("No classifieds present.");
+        }
+        
+        return (
+            <div className="classifieds col-md-3">
+                {classifiedItems}
             </div>
         );
     }
@@ -123,7 +192,8 @@ class NewPost extends  React.Component {
     submit = (values) => {
         fetch(
             "http://localhost:8080/" + this.props.match.params.channel + "/newpost",
-            {   method : 'post',
+            {   
+                method : 'post',
                 mode : 'no-cors',
                 body : JSON.stringify(values)
             }            
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css'; 
 import home from './containers/home';
 import {BrowserRouter, Link, Route, Switch} from 'react-router-dom';
 import {createStore, applyMiddleware} from 'redux';
@@ -19,22 +19,19 @@ class App extends Component {
     render() {
       return (
         <div>
-          <nav className="navbar navbar-expand-lg container-fluid navbar-light">
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-                <a className="navbar-brand mr-auto" href="#" style={{color:"#ff9966"}}>HOODS</a>
-                <ul className="navbar-nav mt-2 mt-lg-0">
-                    <li className="nav-item active">
-                        <a className="nav-link" href="#">My Profile</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Logout</a>
-                    </li>
-                </ul>
-            </div>
-          </nav>
+            <nav className="navbar navbar-expand-lg navbar-light">
+                <div className="container">
+                    <a className="navbar-brand logo mr-auto" href="#" ><span className="color-primary">HOODS</span></a>
+                    <ul className="navbar-nav mt-2 mt-lg-0">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">My Profile</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Logout</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
               <BrowserRouter >
                 <Route path="/" component={home} />
               </BrowserRouter>
